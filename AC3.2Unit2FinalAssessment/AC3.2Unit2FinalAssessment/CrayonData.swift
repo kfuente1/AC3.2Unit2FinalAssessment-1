6b11f8e858b93e5ed6e35bbe61d9e1c4c1b2b9f9
Cristopher Chavez <cristopherchavezny@gmail.com>
2016-10-06 13:57:56 -0400

    I'm done

---
+++ AC3.2Unit2FinalAssessment/AC3.2Unit2FinalAssessment/CrayonData.swift
@@ -0,0 +1,9 @@
+//
+//  CrayonData.swift
+//  AC3.2Unit2FinalAssessment
+//
+//  Created by Cris on 10/6/16.
+//  Copyright © 2016 Cris. All rights reserved.
+//
+
+import Foundation
---
+++ AC3.2Unit2FinalAssessment/AC3.2Unit2FinalAssessment/CrayonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CrayonTableViewCell.swift
+//  AC3.2Unit2FinalAssessment
+//
+//  Created by Cris on 10/6/16.
+//  Copyright © 2016 Cris. All rights reserved.
+//
+
+import UIKit
+
+class CrayonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
