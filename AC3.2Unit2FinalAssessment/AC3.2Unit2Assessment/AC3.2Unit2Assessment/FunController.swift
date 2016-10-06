5da8b76d895bb48620f75c775e84c6cbc937ae13
Marty Avedon <martyavedon@C4Qs-MacBook-Pro-72.local>
2016-10-06 12:39:08 -0400

    added outlets for actions on fun control view

--- AC3.2Unit2FinalAssessment/AC3.2Unit2Assessment/AC3.2Unit2Assessment/FunController.swift
+++ AC3.2Unit2FinalAssessment/AC3.2Unit2Assessment/AC3.2Unit2Assessment/FunController.swift
@@ -20,9 +20,11 @@ class FunController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sliderSlides(_ sender: UISlider) {
+    }
+    @IBAction func stepperSteps(_ sender: UIStepper) {
+    }
+    @IBAction func textfieldTyping(_ sender: UITextField) {
     }
 
 
