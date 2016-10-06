e04b48c463e16e7f7fb3a603e91d4bfe034cdca8
Margaret Ikeda <margaretei@gmail.com>
2016-10-06 13:24:55 -0400

    I'm done

---
+++ AC3.2Unit2FinalAssessment/AC3.2Unit2FinalAssessment/CrayonData.swift
@@ -0,0 +1,92 @@
+//
+//  CrayonData.swift
+//  AC3.2Unit2FinalAssessment
+//
+//  Created by Margaret Ikeda on 10/6/16.
+//  Copyright © 2016 Margaret Ikeda. All rights reserved.
+//
+
+import Foundation
+
+let crayolaColors = [
+    [
+        "hex": "#EFDECD",
+        "name": "Almond",
+        "rgb": "(239, 222, 205)"
+    ],
+    [
+        "hex": "#CD9575",
+        "name": "Antique Brass",
+        "rgb": "(205, 149, 117)"
+    ],
+    [
+        "hex": "#FDD9B5",
+        "name": "Apricot",
+        "rgb": "(253, 217, 181)"
+    ],
+    [
+        "hex": "#78DBE2",
+        "name": "Aquamarine",
+        "rgb": "(120, 219, 226)"
+    ],
+    [
+        "hex": "#87A96B",
+        "name": "Asparagus",
+        "rgb": "(135, 169, 107)"
+    ],
+    [
+        "hex": "#FFA474",
+        "name": "Atomic Tangerine",
+        "rgb": "(255, 164, 116)"
+    ],
+    [
+        "hex": "#FAE7B5",
+        "name": "Banana Mania",
+        "rgb": "(250, 231, 181)"
+    ],
+    [
+        "hex": "#9F8170",
+        "name": "Beaver",
+        "rgb": "(159, 129, 112)"
+    ],
+    [
+        "hex": "#FD7C6E",
+        "name": "Bittersweet",
+        "rgb": "(253, 124, 110)"
+    ],
+    [
+        "hex": "#000000",
+        "name": "Black",
+        "rgb": "(0,0,0)"
+    ],
+    [
+        "hex": "#ACE5EE",
+        "name": "Blizzard Blue",
+        "rgb": "(172, 229, 238)"
+    ],
+    [
+        "hex": "#1F75FE",
+        "name": "Blue",
+        "rgb": "(31, 117, 254)"
+    ],
+    [
+        "hex": "#A2A2D0",
+        "name": "Blue Bell",
+        "rgb": "(162, 162, 208)"
+    ],
+    [
+        "hex": "#6699CC",
+        "name": "Blue Gray",
+        "rgb": "(102, 153, 204)"
+    ],
+    [
+        "hex": "#0D98BA",
+        "name": "Blue Green",
+        "rgb": "(13, 152, 186)"
+    ],
+    [
+        "hex": "#7366BD",
+        "name": "Blue Violet",
+        "rgb": "(115, 102, 189)"
+    ]
+]
