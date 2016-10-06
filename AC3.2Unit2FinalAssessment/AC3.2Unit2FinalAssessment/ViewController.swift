0ddfc1bb99695ecf619068fc5ef028762377f893
Mira Estil <imz.estil@gmail.com>
2016-10-06 10:44:54 -0400

    Unit 2 Final Assessment

---
+++ AC3.2Unit2FinalAssessment/AC3.2Unit2FinalAssessment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AC3.2Unit2FinalAssessment
+//
+//  Created by Ilmira Estil on 10/6/16.
+//  Copyright © 2016 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AC3.2Unit2FinalAssessment/AC3.2Unit2FinalAssessmentTests/AC3_2Unit2FinalAssessmentTests.swift
@@ -0,0 +1,36 @@
+//
+//  AC3_2Unit2FinalAssessmentTests.swift
+//  AC3.2Unit2FinalAssessmentTests
+//
+//  Created by Ilmira Estil on 10/6/16.
+//  Copyright © 2016 C4Q. All rights reserved.
+//
+
+import XCTest
+@testable import AC3_2Unit2FinalAssessment
+
+class AC3_2Unit2FinalAssessmentTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
