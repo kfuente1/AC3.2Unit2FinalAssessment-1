a0caf34b2434a7672419b8befd436e4a4bbb8e4e
Emily Chu <emilychu@C4Qs-MacBook-Pro-101.local>
2016-10-06 12:56:22 -0400

    I'm done

---
+++ AC3.2Unit2FinalAssessment/AC3.2Unit2FinalAssessment/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  AC3.2Unit2FinalAssessment
+//
+//  Created by Emily Chu on 10/6/16.
+//  Copyright © 2016 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var stepper: UIStepper!
+    
+    @IBOutlet weak var Label: UILabel!
+
+    @IBOutlet weak var textField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
