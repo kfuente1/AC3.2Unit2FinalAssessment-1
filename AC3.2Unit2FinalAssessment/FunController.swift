b1d5b754bc9359398f293f394f90b6bbd5b51134
Marty Avedon <martyavedon@C4Qs-MacBook-Pro-72.local>
2016-10-06 12:04:57 -0400

    i just spent an hour fixing the repo

---
+++ AC3.2Unit2FinalAssessment/FunController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AC3.2Unit2FinalAssessment
+//
+//  Created by Marty Avedon on 10/6/16.
+//  Copyright © 2016 Marty Hernandez Avedon. All rights reserved.
+//
+
+import UIKit
+
+class FunController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
