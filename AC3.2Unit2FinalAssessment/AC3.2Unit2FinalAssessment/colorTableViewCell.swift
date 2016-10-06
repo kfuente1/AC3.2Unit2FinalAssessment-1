2936cbf0cf028b6f4a471a08e830637bfb1a9f39
Kadell <Kadell@MacBook.local>
2016-10-06 13:55:42 -0400

    Finished

---
+++ AC3.2Unit2FinalAssessment/AC3.2Unit2FinalAssessment/colorTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  colorTableViewCell.swift
+//  AC3.2Unit2FinalAssessment
+//
+//  Created by Kadell on 10/6/16.
+//  Copyright © 2016 Kadell. All rights reserved.
+//
+
+import UIKit
+
+class colorTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var colorLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+ 
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
